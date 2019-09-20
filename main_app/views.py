43771503c010441bda884ca8f8c314af7c88afc4
Michael Martinez <rusalka2100@gmail.com>
2019-09-20 12:46:39 -0500

    Finished

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Item
+from django.views.generic.edit import CreateView, DeleteView
 
-# Create your views here.
+def index(request):
+    items = Item.objects.all()
+    return render(request, 'index.html',{'items': items})
+
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
+
+class ItemDelete(DeleteView):
+    model = Item
+    success_url = '/'
